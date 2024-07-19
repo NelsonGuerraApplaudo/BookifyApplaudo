a092cdc0d89a1490af449ad73850f5c3b7562448
NelsonGuerra <nguerra@applaudostudios.com>
2024-07-19 11:54:21 -0400

    Initial checking

--- Bookify.Domain/Apartments/Currency.cs
+++ Bookify.Domain/Apartments/Currency.cs
@@ -9,7 +9,7 @@ namespace Bookify.Domain.Apartments;
 public record Currency
 {
     public static readonly Currency Usd = new Currency() { Code = "USD" };
-    public static readonly Currency Eur = new Currency() { Code = "EUR" };
+    public static readonly Currency Eur = new Currency() { Code = "EUR" };   
     public static Currency FromCode(string code) {
         return All.FirstOrDefault(c => c.Code == code) ?? throw new ApplicationException("The Currency is invalid"); }
     public string Code { get; init; }
